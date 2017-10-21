61919ee6e6d5b0feab1c3afa4edd8dcbb059f94e
porkallen <allenms886.com>
2017-10-20 23:47:00 -0700

    Updating configuration

--- main.py
+++ main.py
@@ -5,5 +5,4 @@ import starterbot
 if __name__ == "__main__":
     os.environ.setdefault('SLACK_BOT_TOKEN_DUDLEY', 'xoxb-256688870500-YufJlAqf7bnpNDWONBCkbsxh')
     os.environ.setdefault('SLACK_BOT_TOKEN_PETUNIA', 'xoxb-256652204482-32JtdYAv5f87VTALSr3FKgBA')
-    os.system('python dudleybot.py')
-    dudleybot
+    os.system('/opt/app/env/bin/python /opt/app/dudleybot.py')

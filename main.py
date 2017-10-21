b3c1f9d6d42f4adce73ce29c01c01a1fac31428b
porkallen <allenms886.com>
2017-10-21 01:59:36 -0700

    Updating configuration

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 import sys
 import threading
+import multiprocessing
 
-threads = []
 bot_name = ['dudleybot','petuniabot']
+
 def botexec(str):
     sys.stderr.write('I am '+str+ '\n')
     tmpstr='/opt/app/env/bin/python /opt/app/'+str+'.py'
@@ -11,9 +12,10 @@ def botexec(str):
     return
 
 if __name__ == "__main__":
+    jobs = []
     os.environ.setdefault('SLACK_BOT_TOKEN_DUDLEY', 'xoxb-256688870500-YufJlAqf7bnpNDWONBCkbsxh')
     os.environ.setdefault('SLACK_BOT_TOKEN_PETUNIA', 'xoxb-256652204482-32JtdYAv5f87VTALSr3FKgBA')
-    for name in bot_name:
-        t = threading.Thread(target=botexec(name))
-        threads.append(t)
-        t.start()
+    for i in bot_name:
+        p = multiprocessing.Process(target=botexec,args=(i,))
+        jobs.append(p)
+        p.start()

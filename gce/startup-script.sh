ac6f6b4290041a79b9a5ad3a907e8d7bc60816d3
porkallen <allenms886.com>
2017-10-20 17:33:39 -0700

    Updating configuration

--- gce/startup-script.sh
+++ gce/startup-script.sh
@@ -43,6 +43,8 @@ pip install --upgrade pip virtualenv
 # git requires $HOME and it's not set during the startup script.
 export HOME=/root
 git config --global credential.helper gcloud.sh
+rm -rf /opt/app/hpparodybot
+echo "New Ver"
 git clone https://source.developers.google.com/p/$PROJECTID/r/hpparodybot /opt/app
 
 # Install app dependencies
